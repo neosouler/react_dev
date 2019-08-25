ff9b819c898fbc9e40f41ef2c1c51691dbff380f
JiHwan Hwang <>
2019-08-25 11:56:09 +0900

    Updates

---
+++ precache-manifest.e1a00e5f242ac0caf9e7caecb1c0e4be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a2990826bd5d20418a341a1ae81d379",
+    "url": "/react_dev/index.html"
+  },
+  {
+    "revision": "e310e13bf60bf4d530ce",
+    "url": "/react_dev/static/css/main.cf14e73d.chunk.css"
+  },
+  {
+    "revision": "83a7bd569dcb5e513c51",
+    "url": "/react_dev/static/js/2.f1919187.chunk.js"
+  },
+  {
+    "revision": "e310e13bf60bf4d530ce",
+    "url": "/react_dev/static/js/main.4d6c3a20.chunk.js"
+  },
+  {
+    "revision": "59bf119cafbcdf1f5dc6",
+    "url": "/react_dev/static/js/runtime~main.c9526e7b.js"
+  }
+]);
